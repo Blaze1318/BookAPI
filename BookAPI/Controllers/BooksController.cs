743ea44bb001f02483d6733b7468e7b1b01475f8
Blaze1318 <39748904+Blaze1318@users.noreply.github.com>
2021-04-20 10:45:38 -0400

    First commit

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,70 @@
+﻿using BookAPI.Models;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            var bookExist = await _bookRepository.Get(id);
+            if(bookExist == null)
+            {
+                return BadRequest();
+            }
+            return await _bookRepository.Get(id);
+         
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, value: newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Book>> PutBooks(int id,[FromBody] Book book)
+        {
+           if(id != book.Id)
+            {
+                return BadRequest();
+            }
+            await _bookRepository.Update(book);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> Delete(int id)
+        {
+            var book = await _bookRepository.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            await _bookRepository.Delete(book.Id);
+            return NoContent();
+        }
+    }
+}
